6f685aa192ddb7b685c37981bcfcd6f0e19739ac
miaoxing <miaoxing@bytedance.com>
2021-03-02 17:36:07 +0800

    header plugin
    Change-Id: Ic6f6c3c63c72842cacef726ea7690ab7f0e6c6c6

--- js/background.js
+++ js/background.js
@@ -1,24 +1,53 @@
-function _back() {
-  console.log('background.js')
+var headers = [
+  {
+    key: "Content-type",
+    value: "application/x-www-form-urlencode",
+    enable: false
+  },
+  {
+    key: "Test-Header",
+    value: "按F进入坦克",
+    enable: true
+  }
+];
+
+function getHeaders() {
+  return headers;
 }
-console.log("running...")
 
-
-var views = chrome.extension.getViews({type:'popup'});
-var popup = null
-if(views.length > 0) {
-    popup = views[0];
-    // 直接访问popup的函数
-    popup.test();
+function addHeader(header) {
+  headers.push(header);
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-  console.log(mesasge); // meesage content
-  callback && callback('yes this from background')
+function deleteHeader(index) {
+  headers.splice(index, 1);
+}
+
+function toggleHeader(index) {
+  headers[index].enable = !headers[index].enable;
+}
+
+// 请求拦截器
+// On install 在被安装的时候去初始化
+chrome.runtime.onInstalled.addListener(function(){
+  // 添加事件  
+  chrome.webRequest.onBeforeSendHeaders.addListener(requestHandle, {
+      urls: ["<all_urls>"],// 拦截所有URL的请求
+  },["blocking", "requestHeaders"]); // 阻塞式
+  console.log('load');
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, 'message content', function (res) {
-      console.log('from content:', res);
+// 添加header
+function requestHandle(request) {
+  var requestHeaders = request.requestHeaders;
+  // 添加headers
+  headers.forEach(function(item){
+      if (item.enable) {
+          requestHeaders.push({
+              name: item.key,
+              value: item.value,
+          });
+      }
   });
-})
+  return {requestHeaders};
+}
--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,33 @@
-var backend = chrome.extension.getBackgroundPage();
-backend.test(); // 访问bbackground的函数
+// popup页面被打开时，去后台获取最新header
+window.onload = function () {
+  let backend = chrome.extension.getBackgroundPage();
+  // 调用background方法，获得headers
+  let headers = backend.getHeaders();
+  // 渲染header
+  createElement(headers);
+}
+
+// 增加按钮
+function addHeader() {
+  let backend = chrome.extension.getBackgroundPage();
+  let key = document.querySelector('.key');
+  let value =  document.querySelector('.value');
+  let header = {
+      key: key.value,
+      value: value.value,
+      enable: true
+    }
+  // 调用background方法，新增headers
+  backend.addHeader(header);
+  createElement(header);
+}
+// 启用禁用、删除功能
+function toggleHeader(index) {
+let backend = chrome.extension.getBackgroundPage();
+backend.toggleHeader(index);
+}
+
+function delHeader(index) {
+let backend = chrome.extension.getBackgroundPage();
+backend.deleteHeader(index);
+}
